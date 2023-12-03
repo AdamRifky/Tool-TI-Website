044017f01672ae8648874f360a5ac1a329b41f1e
Gusmang Adi <cybermoon3@gmail.com>
2023-12-04 01:39:47 +0800

    ganti edit = perbarui, dan delete = hapus

--- Pegawai.php
+++ Pegawai.php
@@ -30,13 +30,13 @@
           echo "</td>";
           
           echo "<td>";
-            echo "<a class='btn btn-primary me-md-2 btn-sm' href='index.php?page=pegawai.form&NIP=".$row['NIP']."&action=edit'>Edit</a>";
+            echo "<a class='btn btn-primary me-md-2 btn-sm' href='index.php?page=pegawai.form&NIP=".$row['NIP']."&action=edit'>Perbarui</a>";
             
             // echo "<a class='btn btn-danger me-md-2 btn-sm' href='Pegawai.action.php?NIP=".$row['NIP']."&action=delete'>Delete</a>";
             echo "<form action='pegawai.action.php' method='POST' style ='display:inline;'>";
               echo "<input type='hidden' name='NIP' value='" . $row['NIP'] . "'>";
               echo "<input type='hidden' name='action' value='delete'>";
-              echo "<button type='submit' class='btn btn-danger me-md-2 btn-sm' onclick='return confirm(\"Ingin menghapus data ini?\");'>Delete</button>";
+              echo "<button type='submit' class='btn btn-danger me-md-2 btn-sm' onclick='return confirm(\"Ingin menghapus data ini?\");'>Hapus</button>";
             echo "</form>";
           echo "</td>";
         
