4a61ccdaebd6c2490ad56920840c36d01ceb4b0e
romagna <gedemetra69@gmail.com>
2023-12-11 08:55:41 +0800

    benerin deket deedline

--- dasboard.php
+++ dasboard.php
@@ -3,12 +3,15 @@ require "koneksi.php";
 
 // query data stok
 $data_stok = mysqli_query($koneksi, "SELECT SUM(Stok) AS total_stok FROM tb_barang");
+$data_jumlah = mysqli_query($koneksi, "SELECT SUM(JUMLAH) AS total_jumlah FROM tb_barang");
 
 // mengambil data stok
 $d = mysqli_fetch_assoc($data_stok);
+$j = mysqli_fetch_assoc($data_jumlah);
 
 // menyimpan total stok ke variabel
 $total_stok = $d['total_stok'];
+$total_jumlah = $j['total_jumlah'];
 ?>
 
 <h3>
@@ -24,6 +27,21 @@ $total_stok = $d['total_stok'];
                 <div class="fs-5" style="text-align: center;">
                     <?php
                     echo "$total_stok"
+                    ?>
+                </div>
+                <a class="card-text text-decoration-none text-dark" href="index.php?page=barang">Lihat Detail >></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row text-white mt-5">
+    <div class="col-3">
+        <div class="card bg-info">
+            <div class="card-body">
+                <h5 class="card-title">JUMLAH BARANG</h5>
+                <div class="fs-5" style="text-align: center;">
+                    <?php
+                    echo "$total_jumlah"
                     ?>
                 </div>
                 <a class="card-text text-decoration-none text-dark" href="index.php?page=barang">Lihat Detail >></a>
