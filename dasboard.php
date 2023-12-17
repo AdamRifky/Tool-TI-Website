247a5bf94ab785ddf33d51f396b47a0402fe30a2
romagna <gedemetra69@gmail.com>
2023-12-17 15:20:12 +0800

    dashboard

--- dasboard.php
+++ dasboard.php
@@ -4,14 +4,21 @@ require "koneksi.php";
 // query data stok
 $data_stok = mysqli_query($koneksi, "SELECT SUM(Stok) AS total_stok FROM tb_barang");
 $data_jumlah = mysqli_query($koneksi, "SELECT SUM(JUMLAH) AS total_jumlah FROM tb_barang");
+$data_pegawai = mysqli_query($koneksi, "SELECT count(NIP) AS Jumlah_pegawai FROM tb_pegawai");
+$data_pelanggan = mysqli_query($koneksi, "SELECT count(ID_PELANGGAN) AS Jumlah_pelanggan FROM tb_pelanggan");
 
 // mengambil data stok
 $d = mysqli_fetch_assoc($data_stok);
 $j = mysqli_fetch_assoc($data_jumlah);
+$p = mysqli_fetch_assoc($data_pegawai);
+$n = mysqli_fetch_assoc($data_pelanggan);
+
 
 // menyimpan total stok ke variabel
 $total_stok = $d['total_stok'];
 $total_jumlah = $j['total_jumlah'];
+$total_pegawai =$p['Jumlah_pegawai'];
+$total_pelanggan =$n['Jumlah_pelanggan'];
 ?>
 
 <h3>
@@ -33,6 +40,19 @@ $total_jumlah = $j['total_jumlah'];
             </div>
         </div>
     </div>
+    <div class="col-3">
+        <div class="card bg-success">
+            <div class="card-body">
+                <h5 class="card-title">JUMLAH PEGAWAI</h5>
+                <div class="fs-5" style="text-align: center;">
+                    <?php
+                    echo "$total_pegawai"
+                    ?>
+                </div>
+                <a class="card-text text-decoration-none text-dark" href="index.php?page=pegawai">Lihat Detail >></a>
+            </div>
+        </div>
+    </div>
 </div>
 <div class="row text-white mt-5">
     <div class="col-3">
@@ -45,6 +65,19 @@ $total_jumlah = $j['total_jumlah'];
                     ?>
                 </div>
                 <a class="card-text text-decoration-none text-dark" href="index.php?page=barang">Lihat Detail >></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card bg-success">
+            <div class="card-body">
+                <h5 class="card-title">JUMLAH PELANGGAN</h5>
+                <div class="fs-5" style="text-align: center;">
+                    <?php
+                    echo "$total_pelanggan"
+                    ?>
+                </div>
+                <a class="card-text text-decoration-none text-dark" href="index.php?page=pelanggan">Lihat Detail >></a>
             </div>
         </div>
     </div>
